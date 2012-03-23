9edf247cf198ce91d59e0797c4445856c9c71af8
Richard Sweeney <theorboman@gmail.com>
2012-03-23 19:42:10 +0100

    Eq Height Cols initial commit

---
+++ jQuery/eqHeightCols/eqHeightCols.js
@@ -0,0 +1,36 @@
+(function ($){
+  $.fn.eqHeightCols = function() {
+    return this.each(function() {
+    
+      var $cols = $(this).find('.eq-height'),
+        colHeights = [];
+    
+      $.each($cols, function(){
+        colHeights.push($(this).height());
+      });
+      
+      var tallest = Math.max.apply(Math, colHeights);
+      $cols.css({'height': tallest});
+    
+    });
+  };
+}(jQuery));
+
+jQuery(function($) {
+  
+  $('.eq-height-container').eqHeightCols();
+  
+});
+
+/*
+
+  HTML structure:
+  
+  <div class="eq-height-container">
+    
+    <div class="eq-height">These divs will be the same height<div>
+    <div class="eq-height">These divs will be the same height<div>
+    
+  </div>
+
+*/
