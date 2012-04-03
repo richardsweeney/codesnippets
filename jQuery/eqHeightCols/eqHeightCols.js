3e610349efa4fdf09d6d976940fb39deaa116785
richardsweeney <theorboman@gmail.com>
2012-04-03 08:21:34 +0200

    Better code. Take padding into account.

--- jQuery/eqHeightCols/eqHeightCols.js
+++ jQuery/eqHeightCols/eqHeightCols.js
@@ -1,23 +1,37 @@
 (function ($){
-
   $.fn.eqHeightCols = function() {
-
     return this.each(function() {
     
-      var $cols = this.find('.eq-height'),
-        colHeights = [];
-    
+      var $cols = $(this).find('.eq-height'),
+        colHeights = [],
+        paddingBottom = [],
+        paddingTop = [];
+          
       $.each($cols, function(){
+        paddingBottom.push(parseInt($(this).css('padding-bottom')));
+        paddingTop.push(parseInt($(this).css('padding-top')));
         colHeights.push($(this).height());
       });
       
+      // Need to check padding to ensure heights remain the same
+      var i = 0,
+        l = $cols.length,
+        totalPadding = [];
+      for (; i < l; i++){
+        totalPadding.push(paddingBottom[i] + paddingTop[i]);
+      }
+      
+      totalPadding = (Math.max.apply(Math, totalPadding)) / 2;
+          
       var tallest = Math.max.apply(Math, colHeights);
-      $cols.css({'height': tallest});
+      $cols.css({
+        'height': tallest,
+        'padding-top': totalPadding,
+        'padding-bottom': totalPadding
+      });
     
     });
-    
   };
-  
 }(jQuery));
 
 /*
